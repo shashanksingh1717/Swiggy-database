f2e6c2fcd217d8ffa22cecbdc48c1455ea200d93
shashank singh <shashanksingh@shashanks-MacBook-Air.local>
2026-05-14 19:43:48 +0530

    rename

--- swiggy.sql
+++ swiggy.sql
@@ -1,54 +1,54 @@
 use swiggy;
-select * from restaurants;
-select name , city from restaurants;
-select * from restaurants
+select * from;
+select name , city from database;
+select * from database
 where city = "sirsa";
-select name ,rating from restaurants
+select name ,rating from database
 where rating > 4.5;
-select * from restaurants
+select * from database
 where cost <=300;
-select * from restaurants
+select * from database
 where cuisine = "biryani";
 use swiggy;
-select * from restaurants;
-select name ,rating from restaurants
+select * from database;
+select name ,rating from database
 where rating > 4.5;
-select name , rating from restaurants
+select name , rating from database
 order by rating desc 
 limit 5;
-select name , rating_count from restaurants
+select name , rating_count from database
 where rating_count > 1000;
 select count(*) as total_res
-from restaurants; 
+from database; 
 select avg(cost) as avg_cost
-from restaurants;
-select name ,cost from restaurants
+from database;
+select name ,cost from database
 order by cost asc;
 select cuisine ,min(cost)as min_cost,max(cost)as max_cost
-from restaurants
+from database
 group by cuisine;
 select city ,avg(rating)as avg_rating
-from restaurants
+from database
 group by city;
 select city , count(*) as count_res
-from restaurants
+from database
 group by city;
-select count(city) from restaurants;
+select count(city) from database;
 select count(distinct city)as unique_cities
-from restaurants;
-select * from restaurants
+from database;
+select * from database
 where rating >4 and rating_count > 3000;
-select * from restaurants
+select * from database
 where city = "mumbai" or rating=5;
-select * from restaurants
+select * from database
 where rating_count between 3000 and 10000;
-select * from restaurants
+select * from database
  where city in ( "mumbai","delhi","goa","pune");
- select cuisine ,count(*)as total_restaurants
- from restaurants
+ select cuisine ,count(*)as total_database
+ from database
  group by cuisine
  having count(*)> 100;
- select city ,avg(rating) from restaurants
+ select city ,avg(rating) from database
  group by city
  having avg(rating)> 4.5;
  
